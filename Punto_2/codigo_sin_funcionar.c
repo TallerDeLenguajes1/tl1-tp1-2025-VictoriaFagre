9fca32c7bf0eab13eb3c19ce5bd6b0a3e064ef85
Victoria Fagre <victoriafagre5@gmail.com>
2026-04-04 19:26:24 -0300

    mejora en errores de sintaxis y librerias

--- Punto_2/codigo_sin_funcionar.c
+++ Punto_2/codigo_sin_funcionar.c
@@ -1,4 +1,7 @@
 // codigo_roto.c
+//librerias agregadas por mi: 19:03 04/04/2026 
+#include <stdio.h>
+#include <stdlib.h>
 
 void duplicar_numero(int numero) {
     numero = numero * 2;
@@ -7,18 +10,18 @@ void duplicar_numero(int numero) {
 int main() {
     int valor1;
     int valor2;
-
+    //agregue & a valor1
     printf("Ingrese el primer valor: ");
-    scanf("%d", valor1);
+    scanf("%d", &valor1);
 
     printf("Ingrese el segundo valor: ");
     scanf("%d", &valor2);
-
-    int suma = valor1 + valor2
+    //agregue ; a printf
+    int suma = valor1 + valor2;
     printf("La suma es: %d\n", suma);
 
     duplicar_numero(valor1);
     printf("El primer valor duplicado es: %d\n", valor1);
 
-    return 0
+    return 0;
 }
