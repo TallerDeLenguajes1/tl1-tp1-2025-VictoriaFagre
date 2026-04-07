b4507b03b63fb2282ec5792e178853fb395c366b
Victoria Fagre <victoriafagre5@gmail.com>
2026-04-07 00:17:32 -0300

    depuro codigo entre funciones y cambio sus nombres segun lo que hacen

--- Punto_2/codigo_misterio.c
+++ Punto_2/codigo_misterio.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void f_alpha(int *p) {
+//cambio alpha por invertido ya que invierte el num misterioso
+void f_invertido(int *p) {
     int temp = *p;
     int rev = 0;
     while (temp > 0) {
@@ -10,24 +11,25 @@ void f_alpha(int *p) {
     *p = rev;
 }
 
-void f_beta(int *p) {
+void f_divido_invertido(int *p) {
     *p = *p / 2;
 }
 
-void f_gamma(int *p) {
+void f_sumaInvert_y_cifraInvert(int *p) {
     int temp = *p;
     int suma = 0;
     while (temp > 0) {
         suma = suma + (temp % 10);
         temp = temp / 10;
     }
+    //127+10
     *p = *p + suma;
 }
 
 void procesar_enigma(int *valor_referencia) {
-    f_alpha(valor_referencia);
-    f_beta(valor_referencia);
-    f_gamma(valor_referencia);
+    f_invertido(valor_referencia);
+    f_divido_invertido(valor_referencia);
+    f_sumaInvert_y_cifraInvert(valor_referencia);
 }
 
 int main() {
